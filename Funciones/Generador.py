a03b83c500484881d4c91efa193170f2fa82e396
Luis Enrique <wichobros@gmail.com>
2023-12-05 11:59:50 -0600

    Añadido movimiento de snake,puntuacion y generacion de bolitas

--- Funciones/Generador.py
+++ Funciones/Generador.py
@@ -2,11 +2,47 @@ import cv2
 import numpy as np
 import os
 import random
-
+#from Eliminar import ElimiarEscenarios
 
 
 class GenerarEscenario():
-    
+    def CoordenadasAleatorias(img,Porcentaje,pintar):
+        xreg=[]
+        yreg=[]
+        j=0
+        #Generar numeros aleatorios
+        while (j<Porcentaje):
+            unico=True
+            if pintar=='Camino':
+                x= random.randrange(60,481,60)
+                y= random.randrange(60,481,60)
+                #Sin marco
+                #x= random.randrange(0,541,60)
+                #y= random.randrange(0,541,60)
+            if pintar=='Bolita':
+                x= random.randrange(20,571,60)
+                y= random.randrange(20,571,60)                
+            #verificar si el conjunto de coordenadas ya extiste en el registro
+            for k in range (len (xreg)):
+                if ((x== xreg[k]) and (y==yreg[k])):
+                    unico=False
+                    break
+            #si es unico se registra,pinta la zona y suma un numero al contador
+            if pintar=='Camino':
+                if (unico==True):
+                    j+=1
+                    xreg.append(x)
+                    yreg.append(y)
+                    cv2.rectangle(img,pt1=(x,y),pt2=(x+59,y+59),color=0,thickness=-1)
+            if pintar=='Bolita':
+                if (unico==True and img[y,x] > 0 and img[y+5,x+5] > 0):
+                    j+=1
+                    xreg.append(x)
+                    yreg.append(y)
+                    #cv2.rectangle(img,pt1=(x,y),pt2=(x+19,y+19),color=150,thickness=-1)
+                    cv2.circle(img,center=(x+10,y+10),radius=9,color=200,thickness=-1)
+                    cv2.circle(img,center=(x+10,y+10),radius=9,color=180,thickness=0)
+
     def GenerarImagenes():
         #Elegir cantidad de escenarios o numero aleatorio de escenarios 
         e=int (input ("Eliga una opcion: \n 1.- Escenarios personalizables \n 2.- Escenarios aleatorio \n\n"))
@@ -42,36 +78,23 @@ class GenerarEscenario():
         else:        
             NumEscenarios=random.randint(2,10)
             Porcentaje=random.randint(65,70)
-            
+        
+        Porcentaje=100-Porcentaje
         os.system("cls")
         #Generar imagenes
         for i in range (NumEscenarios):
             print ("Generando imagen "+str(i+1)+"...")
             img = np.zeros((640, 600, 1), np.uint8)
-            cv2.rectangle(img,pt1=(0,601),pt2=(640,640),color=50,thickness=-1)
+            cv2.rectangle(img,pt1=(0,0),pt2=(640,640),color=255,thickness=-1)
+            cv2.rectangle(img,pt1=(0,600),pt2=(640,640),color=50,thickness=-1)
+            
+            Cantidad=30
             
             #Generar pasillos aleatorios
-            #variables donde se guarda datos de x,y para comprobar que el conjunto no se repita 
-            xreg=[]
-            yreg=[]
-            j=0
-            #Generar numeros aleatorios
-            while (j<Porcentaje):
-                unico=True
-                x= random.randrange(0,541,60)
-                y= random.randrange(0,541,60)
-                
-                #verificar si el conjunto de coordenadas ya extiste en el registro
-                for k in range (len (xreg)):
-                    if ((x== xreg[k]) and (y==yreg[k])):
-                        unico=False
-                        break
-                #si es unico se registra,pinta la zona y suma un numero al contador
-                if (unico==True):
-                    j+=1
-                    xreg.append(x)
-                    yreg.append(y)
-                    cv2.rectangle(img,pt1=(x,y),pt2=(x+60,y+60),color=255,thickness=-1)
+            GenerarEscenario.CoordenadasAleatorias(img,Porcentaje,pintar='Camino')
+            #Generar Bolitas
+            GenerarEscenario.CoordenadasAleatorias(img,Cantidad,pintar='Bolita')
+                    
             #Colocar numero de escenario
             if (img[601,575]==255):
                 cv2.putText(img,text="Escenario: {:.0f}".format(i+1),org=(2,630),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=(0,0,0))
@@ -80,12 +103,19 @@ class GenerarEscenario():
       
             #Colocar porcentaje de color blanco
             if (img[601,15]==255):
-                cv2.putText(img,text="Porcentaje blanco: {:.0f}%".format(Porcentaje),org=(2,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=(0,0,0))
+                cv2.putText(img,text="Porcentaje blanco: {:.0f}%".format(100-Porcentaje),org=(2,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=(0,0,0))
             else:
-                cv2.putText(img,text="Porcentaje blanco: {:.0f}%".format(Porcentaje),org=(2,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=(255,255,255))
+                cv2.putText(img,text="Porcentaje blanco: {:.0f}%".format(100-Porcentaje),org=(2,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=(255,255,255))
                   
             cv2.imwrite("Escenarios/Escenario "+str(i+1)+".jpg", img)
             
         print("Imagenes generadas")
+        return NumEscenarios
 
 
+if __name__ == "__main__":
+    # Llamar a la función si se está ejecutando como programa principal
+    os.system("cls")
+    #ElimiarEscenarios.Eliminar()
+    GenerarEscenario.GenerarImagenes()
+    
--- Juego.py
+++ Juego.py
@@ -12,167 +12,156 @@ ElimiarEscenarios.Eliminar()
 os.system("cls")
 
 #Generar escenarios
-GenerarEscenario.GenerarImagenes()
+Cantidad=GenerarEscenario.GenerarImagenes()
 
+Mapa = [[0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],]
 
+Mapa[2][2]=1
+Mapa[1][2]=2
+MapaX=2
+MapaY=2
+
+for fila in Mapa:
+    print(fila)
+input()
 
 img = cv2.imread('Escenarios/Escenario 1.jpg', cv2.IMREAD_GRAYSCALE)  # Leer la imagen
 
 #Crear zona de aparicion aleatoria dependiendo si es blanco o negro
 while True:
-    posicion_inicialX=random.randrange(0,571,30)
-    posicion_inicialY=random.randrange(0,571,30)
+    posicion_inicialX=random.randrange(0,571,20)
+    posicion_inicialY=random.randrange(0,571,20)
     if img[posicion_inicialY,posicion_inicialX] > 0 and img[posicion_inicialY+10,posicion_inicialX+10] > 0:#and img[posicion_inicial+10,posicion_inicial+10]>0:
         break
-    
-    
-    
 zona=1
 #Posiocion x y 
 x = posicion_inicialX
 y = posicion_inicialY
+v = 20
+colorcubo=150
+Perdio=False
 #Posicion x y pasada
-xpas = x
-ypas = y
-#Velocidad
-v = 10
-colorcubo=155
-#Color de pintura
-colorpintar=100
-colorpasado=int(img[y+5,x+5])
-#Colocar cubo en mapa
-cv2.rectangle(img, pt1=(x, y), pt2=(x+10, y+10), color=colorcubo, thickness=-1)
+xpas = []
+ypas = []
 
-pintar=False
-minar=False
-borrar=False
+for i in range(2):
+    xpas.append(x)
+    ypas.append(y)
 
-#Zona de juego 
+c=1
+
+direccion=0
+cv2.rectangle(img, pt1=(x, y), pt2=(x+19, y+19), color=colorcubo, thickness=-1)
+cv2.rectangle(img, pt1=(x, y), pt2=(x+19, y+19), color=colorcubo-20, thickness=0)
+
 while True:
-    if pintar == True:
-        cv2.putText(img,text="Pintar",org=(430,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=250)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=255,thickness=-1)
-    else: 
-        cv2.putText(img,text="Pintar",org=(430,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=0)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=0,thickness=-1)
-        
-    if borrar == True:
-        cv2.putText(img,text="Borrar",org=(490,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=250)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=255,thickness=-1)
-    else: 
-        cv2.putText(img,text="Borrar",org=(490,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=0)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=0,thickness=-1)
-            
-    if minar == True:
-        cv2.putText(img,text="Minar",org=(550,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=250)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=255,thickness=-1)
-    else: 
-        cv2.putText(img,text="Minar",org=(550,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=0)
-        #cv2.rectangle(img,pt1=(580,615),pt2=(590,625),color=0,thickness=-1)
 
-    
-    #Mostrar cambios
+    cv2.rectangle(img,pt1=(430,601),pt2=(640,640),color=50,thickness=-1)
+    cv2.putText(img,text="Puntos: {:.0f}".format(c-1),org=(430,615),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=.5,color=250)
     cv2.imshow('Escenario', img)
     #Esperar tecla
-    key = cv2.waitKey(0)
-    #print (key) #Imprimir numero ASCII de tecla presionada
+    key = cv2.waitKey(300)
     
-    # 27 es el código ASCII de la tecla esc, -1 presionar x en el boton cerrar pantalla
-    if key == 27 or key ==-1:
+    if key == 27 :
+        break
+    if cv2.getWindowProperty('Escenario', cv2.WND_PROP_VISIBLE) < 1:
         break
     
-    #Activar minar con tecla E
-    if key == 108:
-        if minar == True:
-            minar = False
-        else: 
-            minar=True
+    #Arriba
+    if (key==119):
+        if direccion!=3:
+            direccion=1
+    #Izquierda
+    if (key==97):
+        if direccion!=4:
+            direccion=2
+    #Abajo
+    if (key==115):
+        if direccion !=1: 
+            direccion=3
+    #Derecha
+    if (key==100):
+        if direccion!=2:
+            direccion=4
+    #Arriba
+    if(direccion==1) :  
+        y -= v
+    #Izquierda
+    if(direccion==2):
+        x-=v  
+    #Abajo  
+    if(direccion==3):    
+        y+=v
+    #Derecha
+    if(direccion==4):    
+        x+=v
+        
+    if (y<0 or y>=600) or (x<0 or x>=600):
+        #Abajo
+        if (y>=600):
+            print("borde")
+            img = cv2.imread('Escenarios/Escenario '+str(zona)+'.jpg', cv2.IMREAD_GRAYSCALE)
+            y=0
             
-    #Activar pintar con tecla  Q 
-    if key == 106:
-        if pintar == True:
-            pintar = False
-        else: 
-            pintar=True
-            borrar=False
+        #Arriba
+        if (y<0):
+            print("borde")
+            #if Mapa[MapaY-1,MapaX]
+            MapaY-=1
+            img = cv2.imread('Escenarios/Escenario '+str(Mapa[MapaY,MapaX])+'.jpg', cv2.IMREAD_GRAYSCALE)
+            y=580
+            
+        #Derecha
+        if (x>=600):
+            print("borde")
+            img = cv2.imread('Escenarios/Escenario 1.jpg', cv2.IMREAD_GRAYSCALE)
+            x=0
+            
+        #Izquierda   
+        if (x<0):
+            print("borde")
+            img = cv2.imread('Escenarios/Escenario 1.jpg', cv2.IMREAD_GRAYSCALE)
+            x=580
+        #print (x,y)
+        #print(xpas[0],ypas[0])
+        
+        for i in range (len(xpas)):
+            xpas.insert(0,-20)
+            ypas.insert(0,-20)
+            xpas.pop
+            ypas.pop
+    else:
+        if img[y,x]<150 and direccion !=0:
+            x=xpas[0]
+            y=ypas[0]
+            Perdio=True
+            break
+            
 
-    #Borrar 
-    if key == 107:
-        if borrar == True:
-            borrar = False
-        else: 
-            borrar=True
-            pintar=False        
-    #Reiniciar mapa
-    if key == 114:
-        img = cv2.imread('Escenarios/Escenario 1.jpg', cv2.IMREAD_GRAYSCALE)
-        cv2.rectangle(img, pt1=(x, y), pt2=(x + 10, y + 10), color=colorcubo, thickness=-1)
-              
-    #Si presiona WASD entonces...
-    if key == 119 or key == 97 or key == 115 or key == 100:  
-        #Si minar esta activado desactivar comprobaciones     
-        if minar == True:
-                #Arriba
-                if (key==119):
-                    y-=v
-                #Izquierda
-                if (key==97):
-                    x-=v
-                #Abajo
-                if (key==115):
-                    y+=v
-                #Derecha
-                if (key==100):
-                    x+=v
-                    
-        #Si minar esta desactivado activar comprobaciones
+    if (xpas[0]!=x) or (ypas[0]!=y):
+        if img[y+10,x+10]==200:
+            c+=1
         else:
-                # arriba
-                if (key == 119) and (img[y - v, x] > 0) and (img[y - v, x+10] > 0):
-                    
-                    if (y - 1 < 0):
-                        img = cv2.imread('Escenarios/Escenario 2.jpg', cv2.IMREAD_GRAYSCALE)
-                        y=590
-                    else:
-                        y -= v
-                # izquierda
-                if (key == 97) and (x - v >= 0) and (img[y , x - v] > 0) and (img[y+10 , x - v] > 0):
-                    x -= v
-                
-                # abajo
-                if (key == 115) and (y + (v+10) < img.shape[0]) and (img[y + (v+10), x] > 0) and (img[y + (v+10), x+10] > 0):
-                    #print(x,y)
-                    y += v
-                    
-                # derecha
-                if (key == 100) and (x + (v+10) < img.shape[1]) and (img[y, x + (v+10)] > 0) and (img[y+10, x + (v+10)] > 0):
-                    x += v
-        #Si hubo movimiento (comprueba si la posicion anterior y la  posicion nueva es distinta)
-        if (xpas!=x or ypas !=y):
+            xpas.pop()
+            ypas.pop()    
+
+        xpas.insert(0,x)
+        ypas.insert(0,y)
+        cv2.rectangle(img, pt1=(xpas[c], ypas[c]), pt2=(xpas[c]+19 , ypas[c]+19), color=255, thickness=-1)
+        cv2.rectangle(img, pt1=(x, y), pt2=(x + 19, y + 19), color=colorcubo, thickness=-1)
+        cv2.rectangle(img, pt1=(x, y), pt2=(x+19, y+19), color=colorcubo-20, thickness=0)
             
-            #Si pintar esta activado pintar la posicion pasada de color gris
-            #if (pintar == True or borrar==True ):
-
-            #Si pintar esta desctivado pintar posicion pasada para pintar de blanco la zona pasada "Borrar"
-            #else:
-            #print ("color " ,colorpasado)
-            if minar == True and colorpasado==0:
-                cv2.rectangle(img, pt1=(xpas, ypas), pt2=(xpas + 10, ypas + 10), color=255, thickness=-1)
-            else:
-                if borrar==True:
-                    cv2.rectangle(img, pt1=(xpas, ypas), pt2=(xpas+10 , ypas+10), color=255, thickness=-1)
-                else:
-                    cv2.rectangle(img, pt1=(xpas, ypas), pt2=(xpas+10 , ypas+10), color=colorpasado, thickness=-1)
-                
-            colorpasado=int(img[y+5,x+5])
-
-            if pintar==True:
-                cv2.rectangle(img, pt1=(xpas, ypas), pt2=(xpas+10, ypas+10), color=colorpintar, thickness=-1)
-
-            #Pintar el cuadrado en la posicion nueva
-            cv2.rectangle(img, pt1=(x, y), pt2=(x + 10, y + 10), color=colorcubo, thickness=-1)
-            #Guardar posicion pasada
-            #print(colorcubo)
-            xpas=x
-            ypas=y
-cv2.destroyAllWindows()
+if Perdio==True:
+    while True:
+        img = cv2.imread('Imagenes/GameOver.jpg', cv2.IMREAD_GRAYSCALE)
+        cv2.imshow('Escenario', img)
+        key = cv2.waitKey()
+        
+        if key == 27 :
+            break
+        if cv2.getWindowProperty('Escenario', cv2.WND_PROP_VISIBLE) < 1:
+            break
